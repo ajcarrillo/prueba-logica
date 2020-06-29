168d19db67c1ca232d6a0a27ca4d46cfcb880a2a
Andres Carrillo <andresjch2804@gmail.com>
2020-06-29 01:49:15 -0500

    Add validations

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ inquirer.prompt([
   }
 ])
   .then(answers => {
-    let queue = Promise.resolve()
-    for (let i = 0; i < answers.repeat; i++) {
-      queue = queue.then(res => {
-        return program()
+    if (answers.repeat >= 1 && answers.repeat <= 10000) {
+      let queue = Promise.resolve()
+      for (let i = 0; i < answers.repeat; i++) {
+        queue = queue.then(res => {
+          return program()
+        })
+      }
+      queue.then(function () {
+        console.log('see you')
       })
+    } else {
+      console.log('Really dude?')
     }
-    queue.then(function () {
-      console.log('see you')
-    })
   })
 
 
@@ -40,14 +44,19 @@ function program() {
       .then(answers => {
         let {rows, columns} = answers
 
-        if (rows <= columns) {
-          if (rows % 2 === 0) console.log("L")
-          else console.log("R")
-        } else if (rows > columns) {
-          if (columns % 2 === 0) console.log("U")
-          else console.log("D")
+        if (rows >= 1 && rows <= 1000000000 && columns >= 1 && columns <= 1000000000) {
+          if (rows <= columns) {
+            if (rows % 2 === 0) console.log("L")
+            else console.log("R")
+          } else if (rows > columns) {
+            if (columns % 2 === 0) console.log("U")
+            else console.log("D")
+          }
+          resolve('done')
+        } else {
+          console.log('Really dude?')
+          resolve('done')
         }
-        resolve('done')
       })
       .catch(error => {
         if (error.isTtyError) {
